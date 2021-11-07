96377a23d401943bf31f54741f87b8b0bca0d5b4
Varun Khurana <varun.khurana@simplifyvms.com>
2021-11-08 00:22:57 +0530

    new sub commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.example.Login;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws InterruptedException {
+        System.out.println( "Hello World!" );
+        Login lg = new Login();
+        lg.login("","","");
+    }
+}
